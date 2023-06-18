1ebed00e31d5228efad52c73d7dc23f282294959
vanangamudi <selva.developer@gmail.com>
2023-06-19 01:56:41 +0530

    added dependency: arichuvadi and entry point vaayil()

--- mlm/sengo/vaayil.py
+++ mlm/sengo/vaayil.py
@@ -1,43 +1,32 @@
 import argparse
 from arichuvadi import get_letters_coding as _ta
 from sengo import poru
-if __name__ == '__main__':
 
+def vaayil():
     parser = argparse.ArgumentParser('sengo')
-    parser.add_argument('-p', '--paangu',
-                        help='paangu e.g: ^தனி.*',
-                        default=None)
-    
-    parser.add_argument('-s', '--saram',
-                        help='saram to match e.g:தனிச்செம்மொழி தமிழ்',
-                        default=None)
+    parser.add_argument('paangu',
+                        help='paangu e.g: ^தனி.*',)
+
+    parser.add_argument('koappu',
+                        help='file containing text',
+                        nargs="+")
+                        
     
     args = parser.parse_args()
 
-    paangugal, varigal = [], []
-    
-    if args.paangu: paangugal = [args.paangu]
-    if args.saram: varigal = [args.saram]
+    #print(f'======= {args.paangu}  =======')
 
-    if not paangugal:
-        paangugal = open('sothanai-paangugal.txt').read().split('\n')
-        paangugal = [i for i in paangugal if i.strip()]
-        
-    if not varigal:
-        varigal   = open('sothanai-varigal.txt').read().split('\n')
-        varigal   = [i for i in varigal if i.strip()]
-
-    
-    for paangu in paangugal:
-        print(f'======= {paangu}  =======')
-        for saram in varigal:
-            print(f'  ======= {saram}  =======')
-            poruva_aa, thalai, neelam = poru(_ta(paangu),
+    for koappu in args.koappu:
+        for i, saram in enumerate(open(koappu)):
+            saram = saram.strip()
+            #print(f'  ======= {saram}  =======')
+            poruva_aa, thalai, neelam = poru(_ta(args.paangu),
                                              _ta(saram))
 
-            print('>>> "{}" "{}" =?= {}, {}'.format(
-                paangu,
-                saram,
-                poruva_aa,
-                ''.join( _ta(saram) [thalai:thalai+neelam] ) if poruva_aa else ''
-            ))
+            if poruva_aa:
+                print(f'{koappu}:{i} "{args.paangu}" "{saram}" =?= {poruva_aa} ,' \
+                      + ''.join( _ta(saram) [thalai:thalai+neelam] ) if poruva_aa else ''
+                    )
+                
+if __name__ == '__main__':
+    vaayil()
